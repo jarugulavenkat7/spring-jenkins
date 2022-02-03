c4ce57a874beab9cd67434dcdfd2aed11d2d702a
Venkat Sai Jarugula <jarugulavenkat7@gmail.com>
2022-02-02 21:46:30 -0600

    Second commit

--- src/test/java/com/venkat/springjenkins/SpringJenkinsApplicationTests.java
+++ src/test/java/com/venkat/springjenkins/SpringJenkinsApplicationTests.java
@@ -1,13 +1,21 @@
 package com.venkat.springjenkins;
 
+import java.util.logging.Logger;
+import static org.junit.jupiter.api.Assertions.*;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringJenkinsApplicationTests {
+	
+	public static Logger logger=Logger.getLogger(SpringJenkinsApplicationTests.class.getName());
 
 	@Test
 	void contextLoads() {
+		logger.info("First Test case executing");
+		logger.info("Second Test case executing");
+		assertEquals(true,true);
 	}
 
 }
